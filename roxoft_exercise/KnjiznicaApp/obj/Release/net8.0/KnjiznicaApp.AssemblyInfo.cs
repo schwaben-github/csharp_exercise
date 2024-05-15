5f05973261c84a08c5275fab6ee307b702d67440
Tibor W. <schwaben-github@users.noreply.github.com>
2024-05-15 11:31:24 +0200

    Initial commit

--- roxoft_exercise/KnjiznicaApp/obj/Release/net8.0/KnjiznicaApp.AssemblyInfo.cs
+++ roxoft_exercise/KnjiznicaApp/obj/Release/net8.0/KnjiznicaApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("KnjiznicaApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4d4c03e464bba9991a92a566ff3ef56dd241ad5")]
 [assembly: System.Reflection.AssemblyProductAttribute("KnjiznicaApp")]
