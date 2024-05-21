aaa5e10e1839be586575da7432a2ec3aa8ca31c2
Tibor W. <schwaben-github@users.noreply.github.com>
2024-05-22 01:21:13 +0200

    Initial commit Selenium Test from ExecuteAutomation
    Get methods, test result attached

---
+++ SeleniumTest/ExecuteAutomation2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using ExecuteAutomation2;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+// Execute Automation Selenium Test Site not available anymore!
+// Instead, we will use the following test site
+// https://testpages.eviltester.com/styled/validation/input-validation.html
+// Since this site is simialr to the Execute Automation site, we can use the same code
+// The test will fill out the form on the page
+
+public class Program
+{
+    // Create a new instance of the Chrome driver
+    static IWebDriver driver = new ChromeDriver();
+
+    [SetUp]
+    public void Initialize()
+    {
+        // Navigate to URL
+        driver.Navigate().GoToUrl("https://testpages.eviltester.com/styled/validation/input-validation.html");
+        Console.WriteLine("\n Opened URL \n");
+        Thread.Sleep(1000);
+        // Maximize the browser window
+        driver.Manage().Window.Maximize();
+        Thread.Sleep(15000);
+    }
+
+    [Test]
+    public void ExecuteTest()
+    {
+        // First name
+        Console.WriteLine("The value from the first name field is: " + SeleniumGetMethod.GetText(driver, "firstname", "id"));
+        Thread.Sleep(2000);
+
+        // Last name
+        Console.WriteLine("The value from the last name field is: " + SeleniumGetMethod.GetText(driver, "surname", "id"));
+        Thread.Sleep(2000);
+
+        // Age
+        Console.WriteLine("The value from the age field is: " + SeleniumGetMethod.GetText(driver, "age", "id"));
+        Thread.Sleep(2000);
+
+        // Country dropdown
+        Console.WriteLine("The value from the country dropdown is: " + SeleniumGetMethod.GetText(driver, "country", "id"));
+        Thread.Sleep(2000);
+
+        // Notes
+        Console.WriteLine("The note entered is : " + SeleniumGetMethod.GetText(driver, "notes", "id"));
+        Thread.Sleep(5000);
+    }
+
+    [TearDown]
+    public void CleanUp()
+    {
+        // Close the browser
+        driver.Close();
+        Console.WriteLine("Test case closed! \n");
+    }
+}
---
+++ SeleniumTest/ExecuteAutomation2/SeleniumGetMethod.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecuteAutomation2
+{
+    internal class SeleniumGetMethod
+    {
+        public static string GetText(IWebDriver driver, string element, string elementtype)
+        {
+            if (elementtype == "id")
+            {
+                return driver.FindElement(By.Id(element)).GetAttribute("value");
+            }
+            if (elementtype == "name")
+            {
+                return driver.FindElement(By.Name(element)).GetAttribute("value");
+            }
+            else return String.Empty;
+        }
+    }
+}
