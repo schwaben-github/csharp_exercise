d835c6406f4f06a16092d37c68d2dadb27324073
Tibor W. <schwaben-github@users.noreply.github.com>
2024-05-24 01:39:51 +0200

    Initial comit ExecuteAutomation5 from Execute Automation tutorial

---
+++ SeleniumTest/ExecuteAutomation5/Program.cs
@@ -0,0 +1,50 @@
+﻿// Page Navigation of Page Object Model in Selenium with C#
+    
+using System;
+using System.Threading;
+using ExecuteAutomation5;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+// Execute Automation Selenium Test Site not available anymore!
+// Instead, we will use the following test site
+// Index: https://testpages.eviltester.com/styled/index.html
+// Target: https://testpages.eviltester.com/styled/validation/input-validation.html
+
+public class Program
+{
+    [SetUp]
+    public void Initialize()
+    {
+        PropertiesCollection.driver = new ChromeDriver();
+
+        // Navigate to URL
+        PropertiesCollection.driver.Navigate().GoToUrl("https://testpages.eviltester.com/styled/index.html");
+        Console.WriteLine("\n Opened URL. \n");
+        Thread.Sleep(1000);
+        // Maximize the browser window
+        PropertiesCollection.driver.Manage().Window.Maximize();
+        Thread.Sleep(1000);
+    }
+
+    [Test]
+    public void ExecuteTest()
+    {
+        // Click the link to the input form
+        LoginPageObject pageLogin = new LoginPageObject();
+        EAPageObject pageEA = pageLogin.ClickInputForm();
+        Console.WriteLine("Link on the index page clicked. \n");
+
+        pageEA.FillUserForm("Tibor", "Weigand", "52", "Croatia", "This is a test note!");
+        Console.WriteLine("Form filled. \n");
+    }
+
+    [TearDown]
+    public void CleanUp()
+    {
+        // Close the browser
+        PropertiesCollection.driver.Close();
+        Console.WriteLine("Test case closed! \n");
+    }
+}
---
+++ SeleniumTest/ExecuteAutomation5/SeleniumGetSetMethods.cs
@@ -0,0 +1,76 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecuteAutomation5
+{
+    internal class SeleniumGetSetMethods
+    {
+        // Set text methods
+        // EnterText method
+        public static void EnterText(string element, string value, PropertyType elementType)
+        {
+            if (elementType == PropertyType.Id)
+                PropertiesCollection.driver.FindElement(By.Id(element)).SendKeys(value);
+            if (elementType == PropertyType.Name)
+                PropertiesCollection.driver.FindElement(By.Name(element)).SendKeys(value);
+        }
+
+        // Click method
+        public static void Click(string element, PropertyType elementType)
+        {
+            By byElement = null;
+
+            switch (elementType)
+            {
+                case PropertyType.Id:
+                    byElement = By.Id(element);
+                    break;
+                case PropertyType.Name:
+                    byElement = By.Name(element);
+                    break;
+                case PropertyType.CssSelector:
+                    byElement = By.CssSelector(element);
+                    break;
+                case PropertyType.ClassName:
+                    byElement = By.ClassName(element);
+                    break;
+                case PropertyType.LinkText:
+                    byElement = By.LinkText(element);
+                    break;
+            }
+
+            WebDriverWait wait = new WebDriverWait(PropertiesCollection.driver, TimeSpan.FromSeconds(10));
+            IWebElement webElement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(byElement));
+            webElement.Click();
+        }
+
+        // SelectDropDown method
+        public static void SelectDropDown(string element, string value, PropertyType elementType)
+        {
+            if (elementType == PropertyType.Id)
+                new SelectElement(PropertiesCollection.driver.FindElement(By.Id(element))).SelectByText(value);
+            if (elementType == PropertyType.Name)
+                new SelectElement(PropertiesCollection.driver.FindElement(By.Name(element))).SelectByText(value);
+        }
+
+
+        /*// Get text method
+        public static string GetText(string element, PropertyType elementType)
+        {
+            if (elementType == PropertyType.Id)
+            {
+                return PropertiesCollection.driver.FindElement(By.Id(element)).GetAttribute("value");
+            }
+            if (elementType == PropertyType.Name)
+            {
+                return PropertiesCollection.driver.FindElement(By.Name(element)).GetAttribute("value");
+            }
+            else return String.Empty;
+        }*/
+    }
+}
