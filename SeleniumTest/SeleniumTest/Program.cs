0716174abf3e69d2c11bf26827b79868e5b46c52
Tibor W. <schwaben-github@users.noreply.github.com>
2024-05-23 09:19:54 +0200

    Update SeleniumTest from Execute Automation tutorial
    Decreased sleep time

--- SeleniumTest/SeleniumTest/Program.cs
+++ SeleniumTest/SeleniumTest/Program.cs
@@ -7,18 +7,21 @@ namespace SeleniumTest
     {
         static void Main(string[] args)
         {
+
+            // In order to follow the execution in the browser, there are sleep commands added!
+
             // Create a new instance of the Chrome driver
             Console.WriteLine("Test case started! \n");
             IWebDriver driver = new ChromeDriver();
-            Thread.Sleep(5000);
+            Thread.Sleep(1000);
 
             // Navigate to URL
             driver.Navigate().GoToUrl("https://www.google.com/");
             Console.WriteLine("\n Opened URL \n");
-            Thread.Sleep(2000);
+            Thread.Sleep(1000);
             // Maximize the browser window
             driver.Manage().Window.Maximize();
-            Thread.Sleep(5000);
+            Thread.Sleep(1000);
 
             // Find the element that's name attribute is q (Google Search Box)
             IWebElement element = driver.FindElement(By.Name("q"));
@@ -26,7 +29,7 @@ namespace SeleniumTest
             // Type something in the search box
             element.SendKeys("blinkenlights wikipedia");
             Console.WriteLine("Entered text \n");
-            Thread.Sleep(2000);
+            Thread.Sleep(1000);
 
             // Identify the Google Search button
             IWebElement element1 = driver.FindElement(By.Name("btnK"));
@@ -35,7 +38,7 @@ namespace SeleniumTest
             // Click the Search button
             element1.Click();
             Console.WriteLine("Button found and clicked \n");
-            Thread.Sleep(10000);
+            Thread.Sleep(1000);
 
             // Close the browser
             // Optionally, it can be closed by the test case
