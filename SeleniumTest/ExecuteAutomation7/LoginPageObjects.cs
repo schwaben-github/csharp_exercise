0b82db1fc6693d49de4f49dec3b921eced178e9a
Tibor W. <schwaben-github@users.noreply.github.com>
2024-05-24 18:10:13 +0200

    Initial commit ExecuteAutomation7

---
+++ SeleniumTest/ExecuteAutomation7/LoginPageObjects.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecuteAutomation7
+{
+    public class LoginPageObject
+    {
+        public LoginPageObject()
+        {
+            PageFactory.InitElements(PropertiesCollection.driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "inputvalidation")]
+        public IWebElement linkInputForm { get; set; }
+
+        public EAPageObject ClickInputForm()
+        {
+            // Click the link
+            linkInputForm.Click();
+
+            // Return the page object
+            return new EAPageObject();
+        }
+    }
+}
---
+++ SeleniumTest/ExecuteAutomation7/SeleniumGetSetMethods.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecuteAutomation7
+{
+    public static class SeleniumGetSetMethods
+    {
+        // Set text methods
+        // EnterText method
+        public static void EnterText(this IWebElement element, string value)
+        {
+            element.SendKeys(value);
+        }
+
+        // Click method
+        public static void Click(this IWebElement element)
+        {
+            element.Click();
+        }
+
+        // SelectDropDown method
+        public static void SelectDropDown(this IWebElement element, string value)
+        {
+            new SelectElement(element).SelectByText(value);
+        }
+
+
+        // Get text method
+        public static string GetText(this IWebElement element)
+        {
+            return element.GetAttribute("value");
+        }
+
+        // Get text from dropdown method
+        public static string GetTextFromDDL(this IWebElement element)
+        {
+            return new SelectElement(element).AllSelectedOptions.SingleOrDefault().Text;
+        }
+    }
+}
