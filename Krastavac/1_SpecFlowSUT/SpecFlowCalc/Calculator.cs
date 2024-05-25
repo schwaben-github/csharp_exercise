1df71c5dd5bd398e8de9e298e7a14c04e812efb2
Tibor W. <schwaben-github@users.noreply.github.com>
2024-05-25 15:52:02 +0200

    Initial commit Getting started with SpecFlow

---
+++ Krastavac/1_SpecFlowSUT/SpecFlowCalc/Calculator.cs
@@ -0,0 +1,18 @@
+﻿// Changed target framework of SpecFlowCalc.Specs to .NET 8.0 in the properties
+
+using System;
+using System.Collections.Generic;
+
+namespace SpecFlowCalc
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
